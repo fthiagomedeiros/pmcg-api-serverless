664f0744d0d0819dec342dfb35c5f9cef1b479d2
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2020-07-14 02:58:49 -0300

    change for new message

--- src/version/lambda/GetVersion.ts
+++ src/version/lambda/GetVersion.ts
@@ -5,7 +5,7 @@ import { cors } from 'middy/middlewares'
 import { ApiResponse } from '../../response/ApiResponse';
 
 export const handler = middy(async (_event, _context) => {
-    return new ApiResponse().version(200, {"id":1,"newsVersion":2,"videosVersion":21});
+    return new ApiResponse().version(200, {"id":1,"newsVersion":3,"videosVersion":21});
 });
 
 handler.use(cors());
